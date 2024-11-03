7c8ca76bc82b7b1128d66a38dc79dd920ce61af3
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-03 14:56:35 -0300

    refactor(routes): atualizar rotas de notas para usar NoteController

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const NoteController = require('../controllers/NoteController');
 
-router.post('/notes/cadastrar', notes.cadastrarNota);
-router.get('/notes', notes.listarNotas);
-router.get('/notes/:id', notes.obterNota);  
-router.put('/notes/:id', notes.atualizarNota)
-router.delete('/notes/:id', notes.deletarNota)
+router.post('/notes/cadastrar', NoteController.cadastrarNota);
+router.get('/notes', NoteController.listarNotas);
+router.get('/notes/:id', NoteController.obterNota);  
+router.put('/notes/:id', NoteController.atualizarNota)
+router.delete('/notes/:id', NoteController.deletarNota)
 
 module.exports = router;
