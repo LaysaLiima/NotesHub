ab10cd485a32b5f2f5ec68af3cc3e1749515a30e
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-03 19:03:48 -0300

    feat(NoteController, routes): adiciona rotas de geolocalização para notas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const NoteController = require('../controllers/NoteController');
+const geolocalizacoes = require('../controllers/Geolocalizacoes');
 
 router.post('/notes/cadastrar', NoteController.cadastrarNota);
 router.get('/notes', NoteController.listarNotas);
 router.get('/notes/:id', NoteController.obterNota);  
 router.put('/notes/:id', NoteController.atualizarNota)
 router.delete('/notes/:id', NoteController.deletarNota)
+rotas.get('/geolocalizacao', geolocalizacoes.obterLocalizacaoPorIp);
+
 
 module.exports = router;
