201922b43bf991f8f108c15d77eb46dcddc35b5d
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-03 13:14:32 -0300

    feat(NoteController): adiciona função create

---
+++ src/controllers/NoteController.js
@@ -0,0 +1,18 @@
+const knex = require('../conexao');
+const axios = require('axios');
+
+const cadastrarNota = async (req, res) => {
+    const { titulo, conteudo, categoria_id, usuario_id, localizacao } = req.body;
+
+    if (!titulo || !conteudo || !categoria_id || !usuario_id) {
+        return res.status(400).json("Todos os campos são obrigatórios.");
+    }
+
+    try {
+        await knex('notas').insert({ titulo, conteudo, categoria_id, usuario_id, localizacao });
+        return res.status(201).json("Nota cadastrada com sucesso");
+    } catch (error) {
+        console.error('Erro ao cadastrar nota:', error);
+        return res.status(500).json("Erro interno do servidor");
+    }
+};
