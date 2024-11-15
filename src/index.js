7a8d2488454e1f19e624af819d1c6acce28d6605
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-15 11:57:12 -0300

    refactor(core): atualizar importação de rotas no index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-  const routes = require('./routes/app');
+const app = express();
+const router = require('./routes');  
 
-  const app = express();
-  app.use(express.json());
-  app.use('/', routes);
+app.use(express.json());
+app.use('/', router); 
 
-  const iniciarServer = async () => {
-      app.listen(3000, () => {
+const iniciarServer = async () => {
+    app.listen(3000, () => {
         console.log("Servidor iniciado na porta 3000");
-      });
-  };
+    });
+};
 
-  iniciarServer();
+iniciarServer();
